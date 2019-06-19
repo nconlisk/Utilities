b48982359762cf5fc4ba7c8faad37547c0401e64
nconlisk <noecon@gmail.com>
2019-06-19 22:36:30 +0100

    Update setupFolders.py
    Minor change to folder gen script, which removes the need to explicitly type in a file path once the file is run from the location where the folders are to be generated

--- setupFolders.py
+++ setupFolders.py
@@ -1,10 +1,10 @@
 import os
 
-#    Simple utility to create a series of folders in a given directory labelled 2007 - 2019.
 
-path = "C:\\" #input("Enter primary directory name: ")
 
-for i in range(7, 20):   #  Change range and strings to modify for own dates.
+path = os.getcwd() #input("Enter primary directory name: ")
+
+for i in range(7, 20):
 
     if i < 10:
         folder = "200"+str(i)
